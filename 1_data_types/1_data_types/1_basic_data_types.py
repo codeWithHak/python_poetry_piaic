ee5013ac66473f77efb98829b82eec97f4df4ed1
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-07-30 21:40:14 +0500

    learn string methods

---
+++ 1_data_types/1_data_types/1_basic_data_types.py
@@ -0,0 +1,27 @@
+name:str = "Hello"
+print("Name:",(name),"type:",type(name))
+
+age:int = 18
+print("Age:",(age),"type:",type(age))
+
+
+is_married:bool = False 
+print("Is married:",(is_married),"type:",type(is_married))
+
+my_list:list[int] = [1,2,3,4]
+print("My list:",(my_list), "type:",type(my_list))
+
+my_obj:dict = {
+    "name":"huzair",
+    "email":"huzairahmed@gamil.com",
+    "password":123
+}
+print("My Obj:",(my_obj),"type:",type(my_obj))
+
+my_tup:tuple = ("huzair","ahmed","khan")
+print("My Tup:",(my_tup),"type:",type(my_tup))
+
+n:None = 12
+print("None:",(None),"type:",type(None))
+
+
---
+++ 1_data_types/1_data_types/2_string_methods.py
@@ -0,0 +1,14 @@
+name:str = "huzair ahmed khan"
+#some basic string methods
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+
+
+# f-string 
+
+father_name = "mukhtar ahmed khan"
+print(f"Student: {name.title()}, Father: {father_name.title()}")
+print("Name: " + name.title() +'\nFather\'s name: ' + father_name.title())
+
+
