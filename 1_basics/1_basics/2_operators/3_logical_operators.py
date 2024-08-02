98ab0d6607d4603f9f5edf08165d5ab30d2f1898
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-08-02 20:37:15 +0500

    learn bitwise operators

---
+++ 1_basics/1_basics/2_operators/3_logical_operators.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+
+print(a > b and a < b) # False
+print(a > b or a < b) # True
+print(not a ==10) # False
---
+++ 1_basics/1_basics/2_operators/4_bitwise_operators.py
@@ -0,0 +1,18 @@
+a = 10
+b = 5
+
+
+# with & operator
+"""print(bin(a))
+print(bin(b))
+print( a&b , bin(a&b) )"""
+
+# with | operator
+"""print(bin(a))
+print(bin(b))
+print( a|b , bin(a|b) )"""
+
+# with ^ operator
+print(bin(a))
+print(bin(b))
+print( a^b , bin(a^b) )
